192a25710ab10f8cc4f119c862917a3e5626f9ac
Jessika Fernandes <jessika.fernandes@ibm.com>
2019-08-18 21:59:50 -0300

    create controller hello world

---
+++ src/main/java/br/com/alura/forum/controller/HelloControl.java
@@ -0,0 +1,15 @@
+package br.com.alura.forum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloControl {
+
+	@RequestMapping("/")
+	@ResponseBody  	//para que o spring não pesquise o retorno do metódo abaixo como outra página
+	public String hello () {
+		return "Hello World using Spring Boot.";
+	}
+}
