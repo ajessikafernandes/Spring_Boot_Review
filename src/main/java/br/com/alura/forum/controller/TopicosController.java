11443df478402ce268ac92f3a0957a7ab1f1f97a
Jessika Fernandes <jessika.fernandes@ibm.com>
2019-08-19 15:17:42 -0300

    added anotation RestController, and create DTO for Topics

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,30 +3,26 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
 
-@Controller
+@RestController
 public class TopicosController {
 
 	@RequestMapping("/topicos")
-	@ResponseBody
-	public List<Topico> lista(){
+	public List<TopicoDto> lista(){
 		Topico topico = new Topico(
-				"O que é o Spring Framework?", 
-				"O Spring é um framework open source para a plataforma Java criado por Rod Johnson."
-				+ " Trata-se de um framework não intrusivo, baseado nos padrões de projeto "
-				+ "inversão de controle (IoC) e injeção de dependência.",
+				"Dúvida sobre o Spring Framework", 
+				"O que é o Spring Framework?",
 				new Curso("Spring Boot","Programação"));
 		Topico topico2 = new Topico(
-				"O que é MVC?",  
-				"É um padrão de arquitetura de software que separa a representação da informação da "
-				+ "interação do usuário com ela. As camadas s'ao Model, View e Controller",
+				"Dúvida sobre MVC?",  
+				"O que é o MVC?",
 				new Curso("Arquitetura de Software","Programação"));
-		return Arrays.asList(topico, topico2);
+		return TopicoDto.converte(Arrays.asList(topico, topico2));
 	}
 }
