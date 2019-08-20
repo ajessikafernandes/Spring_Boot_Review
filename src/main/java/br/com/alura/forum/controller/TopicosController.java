257be1732e98404c3bd22a5bed77e446a9179eae
Jessika Fernandes <jessika.fernandes@ibm.com>
2019-08-20 11:47:25 -0300

    using pattern repository of jpa

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,28 +1,24 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
 
+	@Autowired
+	public TopicoRepository topicosrepository;
+	
 	@RequestMapping("/topicos")
 	public List<TopicoDto> lista(){
-		Topico topico = new Topico(
-				"Dúvida sobre o Spring Framework", 
-				"O que é o Spring Framework?",
-				new Curso("Spring Boot","Programação"));
-		Topico topico2 = new Topico(
-				"Dúvida sobre MVC?",  
-				"O que é o MVC?",
-				new Curso("Arquitetura de Software","Programação"));
-		return TopicoDto.converte(Arrays.asList(topico, topico2));
+	List<Topico> topicos = topicosrepository.findAll();
+		return TopicoDto.converte(topicos);
 	}
 }
